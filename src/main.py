b765ce408ce984a79de6a194204dbd8e283b8667
Lakshay <79142303+lakshayknows@users.noreply.github.com>
2025-08-21 03:22:45 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,32 +7,38 @@ from langchain_core.prompts import load_prompt
 load_dotenv()
 data = load_data()
 
-# Model from hf
+# Hugging Face model
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.1-8B-Instruct",
     task="text-generation"
 )
 model = ChatHuggingFace(llm=llm)
-# Loading prompts
+
+# Load prompt templates
 prompt1 = load_prompt('template_1.json')
 prompt2 = load_prompt('template_2.json')
+prompt3 = load_prompt('template_3.json')
 
 
 sample_review = data['flat_reviews'].iloc[0]
 
+print(f"Review: '{sample_review}'")
+print("-" * 40)
+
 
 chain1 = prompt1 | model
 result1 = chain1.invoke({"review_text": sample_review})
-
-print(f"Review: '{sample_review}'")
-print("-" * 25)
-print(f"Result from Prompt 1 (Direct):")
+print("\nResult from Prompt 1 (Direct):")
 print(result1.content)
-
 
 
 chain2 = prompt2 | model
 result2 = chain2.invoke({"review_text": sample_review})
-
-print(f"\nResult from Prompt 2 (Role-Playing):")
+print("\nResult from Prompt 2 (Role-Playing):")
 print(result2.content)
+
+
+chain3 = prompt3 | model
+result3 = chain3.invoke({"review_text": sample_review})
+print("\nResult from Prompt 3 (Few-Shot):")
+print(result3.content)
