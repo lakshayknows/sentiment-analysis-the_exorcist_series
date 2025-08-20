e5947e0c8c44e04eebdb034743052fbee5c89528
Lakshay <79142303+lakshayknows@users.noreply.github.com>
2025-08-21 03:23:40 +0530

    Update create_template.py

--- src/create_template.py
+++ src/create_template.py
@@ -15,4 +15,11 @@ template_2 = PromptTemplate(
 )
 template_2.save('template_2.json')
 
+template_3 = PromptTemplate(
+    input_variables=["review_text"],
+    template= "You are analyzing sentiment in movie reviews. Below are labeled examples:\n\nExample 1: 'This movie was terrifying but brilliant.' → POSITIVE\nExample 2: 'Too slow and boring, not scary at all.' → NEGATIVE\nExample 3: 'Decent film, but not as good as the original.' → NEUTRAL\n\nNow classify the following review:\n\nReview: {review_text}\n\nSentiment:"
+    
+)
+
+template_3.save('template_3.json')
 #saving prompts into json files to keep the code clean and increase reusability.
